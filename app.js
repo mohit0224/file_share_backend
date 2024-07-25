51c3ebb16330bc7bb651446f1d79819f2405702a
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-25 15:43:03 +0530

    cors update

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const path = require("path");
 
 const app = express();
 
-app.use(
-	cors({
-		origin: process.env.CORS_ORIGIN,
-		credentials: true,
-	})
-);
+const allowedOrigins = [process.env.CORS_ORIGIN];
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
