b18c2924040f5de02c8a459970ac8a034ffd0445
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-26 16:50:44 +0530

    multer log file

--- controllers/file.controllers.js
+++ controllers/file.controllers.js
@@ -25,6 +25,7 @@ const postFile = async (req, res) => {
 		}
 
 		const file = req.file;
+		console.log(file);
 		if (!file)
 			return res.status(404).json(apiError("Upload file to process !!", false));
 
--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -6,6 +6,7 @@ const storage = multer.diskStorage({
 		cb(null, "./public/");
 	},
 	filename: function (req, file, cb) {
+		console.log(file);
 		cb(null, `${file.originalname}`);
 	},
 });
