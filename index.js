ddd4352116c5a3195b3e1ee1b170d0b41180c422
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-27 00:14:38 +0530

    remove cluster function

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
 const app = require("./app");
 const dbConfig = require("./config/dbConfig");
 const PORT = process.env.PORT || 3050;
-const cluster = require("cluster");
-const os = require("os");
 
-const nCpu = os.cpus().length;
-
-if (cluster.isMaster) {
-	for (let i = 0; i < nCpu; i++) {
-		cluster.fork();
-	}
-	cluster.on("exit", (worker, code, signal) => {
-		console.log(`Worker ${worker.process.pid} died`);
-		cluster.fork();
-	});
-} else {
-	dbConfig()
-		.then(() => {
-			app.listen(PORT, () => {
-				console.log(`Server is listening on port :: ${PORT}`);
-			});
-		})
-		.catch((err) => {
-			console.error(err.message);
-			process.exit(1);
+dbConfig()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is listening on port :: ${PORT}`);
 		});
-}
-
-// dbConfig()
-// 	.then(() => {
-// 		app.listen(PORT, () => {
-// 			console.log(`Server is listening on port :: ${PORT}`);
-// 		});
-// 	})
-// 	.catch((err) => {
-// 		console.error(err.message);
-// 		process.exit(1);
-// 	});
+	})
+	.catch((err) => {
+		console.error(err.message);
+		process.exit(1);
+	});
